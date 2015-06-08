6d3d337ddb2a2df534e821520e3cd271a5d283ae
Michal Foksa <mail@michalfoksa.net>
2015-06-08 21:43:46 +0200

    Forwarder Encapsulation. Mqtt-SN packets are encapsulated according to MQTT-SN Protocol Specification v1.2, chapter 5.5 Forwarder Encapsulation.

--- mqtt-sn-sub.c
+++ mqtt-sn-sub.c
@@ -24,6 +24,7 @@
 
 #include <stdio.h>
 #include <unistd.h>
+#include <getopt.h>
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -42,7 +43,7 @@ const char *mqtt_sn_port = "1883";
 uint16_t topic_id = 0;
 uint16_t keep_alive = 10;
 uint8_t retain = FALSE;
-uint8_t debug = FALSE;
+uint8_t debug = 0;
 uint8_t single_message = FALSE;
 uint8_t clean_session = TRUE;
 uint8_t verbose = FALSE;
@@ -55,23 +56,35 @@ static void usage()
     fprintf(stderr, "\n");
     fprintf(stderr, "  -1             exit after receiving a single message.\n");
     fprintf(stderr, "  -c             disable 'clean session' (store subscription and pending messages when client disconnects).\n");
-    fprintf(stderr, "  -d             Enable debug messages.\n");
+    fprintf(stderr, "  -d             Increase debug level by one. -d can occur multiple times.\n");
     fprintf(stderr, "  -h <host>      MQTT-SN host to connect to. Defaults to '%s'.\n", mqtt_sn_host);
     fprintf(stderr, "  -i <clientid>  ID to use for this client. Defaults to 'mqtt-sn-tools-' with process id.\n");
     fprintf(stderr, "  -k <keepalive> keep alive in seconds for this client. Defaults to %d.\n", keep_alive);
     fprintf(stderr, "  -p <port>      Network port to connect to. Defaults to %s.\n", mqtt_sn_port);
     fprintf(stderr, "  -t <topic>     MQTT topic name to subscribe to.\n");
     fprintf(stderr, "  -T <topicid>   Pre-defined MQTT-SN topic ID to subscrube to.\n");
+    fprintf(stderr, "  --fe           Enables Forwarder Encapsulation. Mqtt-sn packets are encapsulated according to MQTT-SN Protocol Specification v1.2, chapter 5.5 Forwarder Encapsulation.\n" );
+    fprintf(stderr, "  --wlnid        If Forwarder Encapsulation is enabled, wireless node ID for this client. Defaults to process id.\n" );
     fprintf(stderr, "  -v             Print messages verbosely, showing the topic name.\n");
     exit(-1);
 }
 
 static void parse_opts(int argc, char** argv)
 {
+
+    static struct option long_options[] =
+    {
+        {"fe" ,    no_argument ,       0 , 'f' } ,
+        {"wlnid" , required_argument , 0 , 'w' } ,
+        {0, 0, 0, 0}
+    } ;
+
     int ch;
+    /* getopt_long stores the option index here. */
+    int option_index = 0;
 
     // Parse the options/switches
-    while ((ch = getopt(argc, argv, "1cdh:i:k:p:t:T:v?")) != -1)
+    while ((ch = getopt_long (argc , argv , "1cdh:i:k:p:t:T:v?" , long_options , &option_index )) != -1)
         switch (ch) {
         case '1':
             single_message = TRUE;
@@ -82,7 +95,7 @@ static void parse_opts(int argc, char** argv)
             break;
 
         case 'd':
-            debug = TRUE;
+            debug++ ;
             break;
 
         case 'h':
@@ -107,6 +120,14 @@ static void parse_opts(int argc, char** argv)
 
         case 'T':
             topic_id = atoi(optarg);
+            break;
+
+        case 'f':
+            mqtt_sn_enable_frwdencap() ;
+            break;
+
+        case 'w' :
+            mqtt_sn_set_frwdencap_parameters( (uint8_t*)optarg , strlen(optarg) ) ;
             break;
 
         case 'v':
@@ -145,13 +166,15 @@ static void termination_handler (int signum)
         break;
     }
 
-    // Signal the main thead to stop
+    // Signal the main thread to stop
     keep_running = FALSE;
 }
 
 int main(int argc, char* argv[])
 {
     int sock, timeout;
+
+    mqtt_sn_disable_frwdencap() ;
 
     // Parse the command-line options
     parse_opts(argc, argv);
