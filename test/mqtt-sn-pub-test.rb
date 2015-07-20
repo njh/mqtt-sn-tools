ecfe4c1715202b96fe61e119573ea1f851ee79f7
Nicholas Humfrey <njh@aelius.com>
2015-07-20 22:32:03 +0100

    Added tests for publishing QoS 0

--- test/mqtt-sn-pub-test.rb
+++ test/mqtt-sn-pub-test.rb
@@ -11,7 +11,7 @@ class MqttSnPubTest < MiniTest::Unit::TestCase
           'mqtt-sn-pub',
           '-q' => -1,
           '-T' => 10,
-          '-m' => 'Hello',
+          '-m' => 'test_publish_qos_n1',
           '-p' => fs.port
         )
       end
@@ -20,8 +20,68 @@ class MqttSnPubTest < MiniTest::Unit::TestCase
     assert_empty @cmd_result
     assert_equal 10, @packet.topic_id
     assert_equal :predefined, @packet.topic_id_type
-    assert_equal 'Hello', @packet.data
+    assert_equal 'test_publish_qos_n1', @packet.data
     assert_equal -1, @packet.qos
+  end
+
+  def test_publish_qos_0
+    fake_server do |fs|
+      @packet = fs.wait_for_publish do
+        @cmd_result = run_cmd(
+          'mqtt-sn-pub',
+          '-q' => 0,
+          '-t' => 'topic',
+          '-m' => 'test_publish_qos_0',
+          '-p' => fs.port
+        )
+      end
+    end
+
+    assert_empty @cmd_result
+    assert_equal 1, @packet.topic_id
+    assert_equal :normal, @packet.topic_id_type
+    assert_equal 'test_publish_qos_0', @packet.data
+    assert_equal 0, @packet.qos
+  end
+
+  def test_publish_qos_0_short
+    fake_server do |fs|
+      @packet = fs.wait_for_publish do
+        @cmd_result = run_cmd(
+          'mqtt-sn-pub',
+          '-q' => 0,
+          '-t' => 'TT',
+          '-m' => 'test_publish_qos_0_short',
+          '-p' => fs.port
+        )
+      end
+    end
+
+    assert_empty @cmd_result
+    assert_equal 'TT', @packet.topic_id
+    assert_equal :short, @packet.topic_id_type
+    assert_equal 'test_publish_qos_0_short', @packet.data
+    assert_equal 0, @packet.qos
+  end
+
+  def test_publish_qos_0_predefined
+    fake_server do |fs|
+      @packet = fs.wait_for_publish do
+        @cmd_result = run_cmd(
+          'mqtt-sn-pub',
+          '-q' => 0,
+          '-T' => 127,
+          '-m' => 'test_publish_qos_0_predefined',
+          '-p' => fs.port
+        )
+      end
+    end
+
+    assert_empty @cmd_result
+    assert_equal 127, @packet.topic_id
+    assert_equal :predefined, @packet.topic_id_type
+    assert_equal 'test_publish_qos_0_predefined', @packet.data
+    assert_equal 0, @packet.qos
   end
 
 end
