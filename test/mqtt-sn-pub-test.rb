0b91b104b7fadbe789971f713409d3e4362e6621
Nicholas Humfrey <njh@aelius.com>
2015-07-22 17:57:08 +0100

    Test for publishing empty/null message

--- test/mqtt-sn-pub-test.rb
+++ test/mqtt-sn-pub-test.rb
@@ -114,4 +114,24 @@ class MqttSnPubTest < Minitest::Test
     assert_equal true, @packet.retain
   end
 
+  def test_publish_qos_0_empty
+    fake_server do |fs|
+      @packet = fs.wait_for_publish do
+        @cmd_result = run_cmd(
+          'mqtt-sn-pub',
+          ['-r', '-n',
+          '-t', 'topic',
+          '-p', fs.port]
+        )
+      end
+    end
+
+    assert_empty @cmd_result
+    assert_equal 1, @packet.topic_id
+    assert_equal :normal, @packet.topic_id_type
+    assert_equal '', @packet.data
+    assert_equal 0, @packet.qos
+    assert_equal true, @packet.retain
+  end
+
 end
