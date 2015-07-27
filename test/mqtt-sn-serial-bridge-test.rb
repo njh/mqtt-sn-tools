54d4c62d58d27beaaa87e7f74db8901d99e3f4b1
Nicholas Humfrey <njh@aelius.com>
2015-07-27 09:50:52 +0100

    Added skeleton test for mqtt-sn-serial-bridge

---
+++ test/mqtt-sn-serial-bridge-test.rb
@@ -0,0 +1,12 @@
+$:.unshift(File.dirname(__FILE__))
+
+require 'test_helper'
+
+class MqttSnSerialBridgeTest < Minitest::Test
+
+  def test_usage
+    @cmd_result = run_cmd('mqtt-sn-serial-bridge', '-?')
+    assert_match /^Usage: mqtt-sn-serial-bridge/, @cmd_result[0]
+  end
+
+end
