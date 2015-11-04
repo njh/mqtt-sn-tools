2efafc1dd5884d41516710f31797183a80d09b57
Nicholas Humfrey <njh@aelius.com>
2015-11-04 08:52:59 +0000

    Added test for duration field being set on a Disconnect packet

--- test/mqtt-sn-pub-test.rb
+++ test/mqtt-sn-pub-test.rb
@@ -353,4 +353,24 @@ class MqttSnPubTest < Minitest::Test
     assert_match /nodename nor servname provided, or not known/, @cmd_result[0]
   end
 
+  def test_disconnect_duration_warning
+    fake_server do |fs|
+      def fs.handle_disconnect(packet)
+        MQTT::SN::Packet::Disconnect.new(
+          :duration => 10
+        )
+      end
+
+      @cmd_result = run_cmd(
+        'mqtt-sn-pub',
+        '-T' => 10,
+        '-m' => 'message',
+        '-p' => fs.port,
+        '-h' => fs.address
+      )
+    end
+
+    assert_match /DISCONNECT warning. Gateway returned duration in disconnect packet/, @cmd_result[0]
+  end
+
 end
