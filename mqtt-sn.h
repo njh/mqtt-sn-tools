865ba462e5861d74e105f0a1d1657d8d4b92fdff
Nicholas Humfrey <njh@aelius.com>
2017-07-31 18:45:07 +0100

    Adjusted the description of mqtt-sn.c and mqtt-sn.h

--- mqtt-sn.h
+++ mqtt-sn.h
@@ -1,6 +1,6 @@
 /*
-  Basic MQTT-SN client library
-  Copyright (C) 2013-2016 Nicholas Humfrey
+  Common functions used by the MQTT-SN Tools
+  Copyright (C) 2013-2017 Nicholas Humfrey
 
   Permission is hereby granted, free of charge, to any person obtaining
   a copy of this software and associated documentation files (the
