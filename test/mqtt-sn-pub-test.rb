e0b9d5be5940b1b126f7d469c19eb38ebc7169b9
Nicholas Humfrey <njh@aelius.com>
2020-05-08 18:06:52 +0100

    Deleted duplicate test

--- test/mqtt-sn-pub-test.rb
+++ test/mqtt-sn-pub-test.rb
@@ -198,26 +198,6 @@ class MqttSnPubTest < Minitest::Test
     assert_equal(0, @packet.qos)
   end
 
-  def test_publish_from_file
-    fake_server do |fs|
-      @packet = fs.wait_for_packet(MQTT::SN::Packet::Publish) do
-        @cmd_result = run_cmd(
-          'mqtt-sn-pub',
-          ['-t', 'topic',
-          '-f', 'test.txt',
-          '-p', fs.port,
-          '-h', fs.address]
-        )
-      end
-    end
-
-    assert_empty(@cmd_result)
-    assert_equal(1, @packet.topic_id)
-    assert_equal(:normal, @packet.topic_id_type)
-    assert_equal('The is the contents of test.txt', @packet.data)
-    assert_equal(0, @packet.qos)
-  end
-
   def test_publish_from_file_too_big
     fake_server do |fs|
       @packet = fs.wait_for_packet(MQTT::SN::Packet::Publish) do
