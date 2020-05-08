4d3a1b0b26e050d8d6033e3cd260257b65b50c45
Nicholas Humfrey <njh@aelius.com>
2020-05-08 18:09:16 +0100

    Added tests for tools setting a default client ID

--- test/mqtt-sn-pub-test.rb
+++ test/mqtt-sn-pub-test.rb
@@ -14,6 +14,25 @@ class MqttSnPubTest < Minitest::Test
     assert_match(/^Usage: mqtt-sn-pub/, @cmd_result[0])
   end
 
+  def test_default_client_id
+    fake_server do |fs|
+      @packet = fs.wait_for_packet(MQTT::SN::Packet::Connect) do
+        @cmd_result = run_cmd(
+          'mqtt-sn-pub',
+          '-T' => 10,
+          '-m' => 'message',
+          '-p' => fs.port,
+          '-h' => fs.address
+        )
+      end
+    end
+
+    assert_empty(@cmd_result)
+    assert_match(/^mqtt-sn-tools-(\d+)$/, @packet.client_id)
+    assert_equal(10, @packet.keep_alive)
+    assert_equal(true, @packet.clean_session)
+  end
+
   def test_custom_client_id
     fake_server do |fs|
       @packet = fs.wait_for_packet(MQTT::SN::Packet::Connect) do
